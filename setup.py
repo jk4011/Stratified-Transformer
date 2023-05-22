7f64ee6b717b32620647304c7dcebbaa393891a1
JinHyeok <jinhyuk@unist.ac.kr>
2023-05-22 08:44:06 +0000

    make into package

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="stratified",
+    version="0.0.0",
+    description="stratified",
+    long_description_content_type="text/markdown",
+    url="https://github.com/jk4011/Stratified-Transformer",
+    author="Jinhyeok Kim",
+    author_email="jinhyuk@unist.ac.kr",
+    packages=["jhutil"],
+    python_requires=">=3.7, <4",
+    project_urls={
+        "Source": "https://github.com/jk4011/Stratified-Transformer",
+    },
+)
